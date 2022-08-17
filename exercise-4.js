b36ae247939b64ddac17a4c834d76f7a46d976e9
Dmitriy Koltovich <92305250+KoltovichDV@users.noreply.github.com>
2022-08-17 13:47:58 +0300

    3, 4, 5 задания

--- exercise-4.js
+++ exercise-4.js
@@ -4,3 +4,15 @@
 // Напишите функцию, которая принимает два числа. Каждую секунду необходимо выводить числа в интервале от первого до второго принятого числа. Используйте setInterval. Например, пользователь ввёл числа 5 и 15. Каждую секунду в консоль должно печататься число, начиная с 5 и заканчивая 15 (всего 11 чисел: 5 6 7 8 9 10 11 12 13 14 15).
 
 // РЕШЕНИЕ:
+function writeNumberRange(start, end) {
+    let counter = start;
+    const timer = setInterval(() => {
+        if (counter > end) {
+            clearInterval(timer);
+            return;
+        }
+        console.log(counter);
+        counter++;
+    }, 1000);
+}
+writeNumberRange(5, 15)
